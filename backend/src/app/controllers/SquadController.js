a95b3080cb160a0ff370e13e757a2b5f5167810e
joão <joaomarcos70outlook.com>
2020-06-09 19:35:56 -0300

    implementando listagem de squads

--- backend/src/app/controllers/SquadController.js
+++ backend/src/app/controllers/SquadController.js
@@ -1,22 +1,43 @@
 import db from '../../database/connection';
 
 class SquadController {
+    async index(req, res){
+        const id_usuario = req.idUsuario;
+        const nome = req.body.nome;
+
+        // teste de verificação para saber se o usuario não é do tipo funcionario
+        if(tipo_usuario === 3){
+            return res.status(401).json({ mensagem:"não autorizado"});
+        }
+        
+        const listaSquad = await db("squad").select({
+            nome,
+            id_criador: id_usuario
+        });
+
+return res.status(200).json({mensagem: "listagem feita com sucesso"});
+
+
+
+    }
+
+    
     async store(req, res){
         const id_usuario =  req.idUsuario;
         const tipo_usuario = req.tipoUsuario;
         const nome = req.body.nome;
 
-        console.log(nome);
+        // teste de verificação para saber se o usuario não é do tipo funcionario
         if(tipo_usuario === 3){
             return res.status(401).json({ mensagem:"não autorizado"});
         }
-
+        //verifica se o squad a ser criado ja não existe no banco de dados
         const verificaNome = await db("squad").where({
             nome,
             id_criador: id_usuario       
         }).first();
-        console.log(verificaNome);
 
+        //caso exista retorna status 400
         if(verificaNome){
             return res.status(400).json({ mensagem:`A Squad ${nome}, ja está cadastrada `});
         }
