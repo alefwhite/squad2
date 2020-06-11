c1bf9393f99c0fa8211f4d8d51f83afed2ef131b
Alef <alefwhite@hotmail.com>
2020-06-10 23:03:29 -0300

    Listagem de tarefas da squad que o usuário está vinculado

--- backend/src/app/controllers/NotificacaoSquadTarefaController.js
+++ backend/src/app/controllers/NotificacaoSquadTarefaController.js
@@ -1,23 +1,45 @@
+import db from '../../database/connection';
 import NotificacaoSquadTarefa from '../models/NotificacaoSquadTarefa';
+import formatarDataBr from '../../utils/formatarDataBr';
 
 class NotificacaoSquadTarefaController {
     async index(req, res) {
-        const notificacoes = await NotificacaoSquadTarefa.find({
-            // squad: req.idUsuario,
-            lido: false
-        })
-        .sort({ createdAt: 'desc' })
-        .limit(20);
+        const notificacoes_squad = new Array();
+        const id_usuario = req.idUsuario;
+        const squadsUsuario = await db("squad_usuario").where({ id_usuario });
 
-        return res.json(notificacoes);
+        if(!squadsUsuario) {
+            return res.status(400).json({ mensagem: "Usuário não pertence a nenhuma squad!" });
+        }   
+
+        for(let id of squadsUsuario ) {
+            const notificacoes = await NotificacaoSquadTarefa.find({
+                squad: id.id_squad,
+                lido: false
+            })
+            .sort({ createdAt: 'desc' })
+            .limit(10);
+
+            for(let notificacao of notificacoes) {
+                notificacoes_squad.unshift(notificacao);
+            }
+            
+        }
+
+        const notificacoesSquadOrdenada = notificacoes_squad.sort((a,b) => {
+            return b.createdAt - a.createdAt;
+        })
+
+
+        return res.json(notificacoesSquadOrdenada);
     }
 
     async store(req, res) {
-        const { id_usuario } = req.body
+        const { id_squad } = req.body
 
         const notificacao = await NotificacaoSquadTarefa.create({
-            conteudo: `Você tem uma nova tarefa ${new Date()}`,
-            user: id_usuario,
+            conteudo: `Você tem uma nova tarefa - ${formatarDataBr(new Intl.DateTimeFormat('pt-BR').format(new Date))}`,
+            squad: id_squad,
         });
 
         return res.json(notificacao);
