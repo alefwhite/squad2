cb3ce1c8cacf80a9269be84018236dd60617577c
Fabio <Fabio.dantascruz@hotmail.com>
2020-06-11 00:13:15 -0300

    implementação dos metodos do crud para a tabela projeto

--- backend/src/app/controllers/ProjetoController.js
+++ backend/src/app/controllers/ProjetoController.js
@@ -2,25 +2,22 @@ import db from '../../database/connection';
 
 class ProjetoController {
     async store(req, res) {
-        const userId =  req.idUsuario;
+        const userId = req.idUsuario;
         const tipoUsuario = req.tipoUsuario;
-        const { nome, descricao, data_inicial, data_final} = req.body;
-        
-        if(tipoUsuario === 3) {
-            return res.status(401).json({ error: "Não autorizado!"});
+        const { nome, descricao, data_inicial, data_final } = req.body;
+
+        if (tipoUsuario === 3) {
+            return res.status(401).json({ error: "Não autorizado!" });
         }
-        console.log(nome.toLowerCase());
-        
         const projeto = await db("projeto")
-            .where({ 
+            .where({
                 nome,
                 id_criador: userId
             })
             .first();
 
-
-        if(projeto) {
-            return res.status(400).json({ error: "Você já tem um projeto cadastrado com esse nome!"});
+        if (projeto) {
+            return res.status(400).json({ error: "Você já tem um projeto cadastrado com esse nome!" });
         }
 
         try {
@@ -32,13 +29,12 @@ class ProjetoController {
                 id_criador: userId
 
             });
-            console.log(novo_projeto)
-            if(novo_projeto) {
-                return res.json({ mensagem: "Projeto cadastrado com sucesso!"});
+            if (novo_projeto) {
+                return res.json({ mensagem: "Projeto cadastrado com sucesso!" });
             }
-            
+
         } catch (error) {
-            return res.status(400).json({ error: "Erro no cadastro do projeto!" });
+            return res.status(400).json({ error: "Erro no cadastro do projeto!", error });
         }
 
 
@@ -46,18 +42,75 @@ class ProjetoController {
 
     async index(req, res) {
         //TODO - Criar metodo para listar os projetos
+        const userId = req.idUsuario;
+        const tipoUsuario = req.tipoUsuario;
+        console.log(userId);
+        if (tipoUsuario === 3) {
+            return res.status(401).json({ error: "Não autorizado!" });
+        }
+
+        const projeto = await db("projeto")
+            .where({
+                id_criador: userId
+            })
+
+        return res.status(200).json({ projetos: projeto });
+
     }
 
 
-     async update(req, res) {
+    async update(req, res) {
         //TODO - Criar metodo para atualizar o projetos
-     }
+        const userId = req.idUsuario;
+        const tipoUsuario = req.tipoUsuario;
+        const { nome, descricao, data_inicial, data_final, id_projeto } = req.body;
 
-     
-     async delete(req, res) {
-         //TODO - Criar metodo para remover um projeto
+        if (tipoUsuario === 3) {
+            return res.status(401).json({ error: "Não autorizado!" });
 
-     }
+        }
+
+        try {
+            const atualizaProjeto = await db("projeto")
+                .where({
+                    id_projeto,
+                }).update({
+                    nome: nome.toLowerCase(),
+                    descricao: descricao.toLowerCase(),
+                    data_inicial,
+                    data_final,
+                });
+            if (atualizaProjeto) {
+                return res.status(200).json({ mensagem: "Projeto atualizado com sucesso" });
+            }
+        } catch (error) {
+            return res.status(400).json({ error: "Erro na atualização do projeto!", });
+        }
+    }
+
+    async delete(req, res) {
+        //TODO - Criar metodo para remover um projeto
+
+        const userId = req.idUsuario;
+        const tipoUsuario = req.tipoUsuario;
+        const { id_projeto } = req.body;
+
+        if (tipoUsuario === 3) {
+            return res.status(401).json({ error: "Não autorizado!" });
+        } try {
+            const deletaProjeto = await db("projeto")
+                .where({
+                    id_projeto,
+                }).del();
+            if (deletaProjeto) {
+                return res.status(200).json({ mensagem: "Projeto removido com sucesso" });
+            }
+        } catch (error) {
+            return res.status(400).json({ error: "Erro em remover o projeto!", });
+        }
+
+
+    }
 
 }
 
