1951ed882b168c75d2e74e189e39387232bee21e
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-06-12 09:38:22 -0300

    Editando estrutura base do frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/App';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={App}/>
+  </Switch>
+  </BrowserRouter>
+  
+  ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ frontend/src/pages/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+export default function App(){
+  
+  return(
+    <div>
+    <h1>INICIO</h1>
+    </div>
+  )
+}
+
+
+
+
