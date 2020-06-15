ba1a9a4c7ad845fa47471ab72c9a5eebb28fd0f3
joão <joaomarcos70outlook.com>
2020-06-15 14:50:26 -0300

    inserção do listar Usuarios na UsuarioController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -20,6 +20,7 @@ routes.post('/session', SessionController.store);
 routes.post('/usuario', UsuarioController.store);
 
 // ConviteUsuario
+routes.get('/convite',Authentication.store, ConviteUsuarioController.index)
 routes.get('/convite', Authentication.store, ConviteUsuarioController.show);
 routes.post('/convite', ConviteUsuarioController.store);
 
