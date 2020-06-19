42784bf9c87090bf308b5638016b33bf5eddaaad
joão <joaomarcos70outlook.com>
2020-06-19 09:32:24 -0300

    removendo link de "esqueceu sua senha"

--- frontend/src/pages/Gestor/cadastro.js
+++ frontend/src/pages/Gestor/cadastro.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
@@ -27,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function SignIn() {
+export default function Cadastrar() {
     const classes = useStyles();
 
     return (
@@ -98,14 +96,6 @@ export default function SignIn() {
                     >
                         Cadastrar
                     </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-              </Link>
-                        </Grid>
-
-                    </Grid>
                 </form>
             </div>
         </Container>
