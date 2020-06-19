b9bfe6b9c8d982b967bbad247a8f62fcb89b1cbc
joão <joaomarcos70outlook.com>
2020-06-19 16:48:43 -0300

    tela finalizada

--- frontend/src/pages/Gestor/cadastro.js
+++ frontend/src/pages/Gestor/cadastro.js
@@ -1,107 +1,139 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+
+
 
 const useStyles = makeStyles((theme) => ({
     container: {
         background: 'linear-gradient(180deg, #303030 0%, #000000 100%)',
+        width: '100vw',
     },
     campos: {
         borderRadius: '20px',
-        background: 'white',
+        width: '77vw',
+        background: '#FDFCFC',
         border: '2px solid #7A57EA',
-        marginTop: theme.spacing(5),
-    },
+        marginTop: '0px',
+        marginBottom: '0px',
 
+    },
     paper: {
         height: '100%',
-        marginTop: theme.spacing(8),
+        paddingTop: theme.spacing(12),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
     },
-
-
     form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
+        width: '77vw',
+        height: '100vh'
     },
     submit: {
         background: '#FE963D',
         borderRadius: '10px',
         width: '180px',
         height: '40px',
-        margin: theme.spacing(5, 10),
+        marginLeft: '60px',
+        justifyContent: 'center',
+        marginTop: '80px'
     },
+
+    texto: {
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: '17px',
+        lineHeight: '17px',
+        paddingLeft: '20px'
+    },
+
 }));
+
+
+
+function cadastrarUsuario() {
+    alert("Cadastrando usuário");
+}
 
 export default function Cadastrar() {
     const classes = useStyles();
 
-    return (
 
-        <Container className={classes.container} component="main" maxWidth="xs">
+
+    return (
+        <div className={classes.container}>
             <CssBaseline />
             <div className={classes.paper}>
                 <form className={classes.form} noValidate>
+                    <h2 className={classes.texto} style={{ color: '#7A57EA' }}>
+                        Nome Completo
+                    </h2>
                     <TextField className={classes.campos}
                         variant="outlined"
                         margin="normal"
                         required
-                        fullWidth
                         id="nome"
                         label="Nome Completo"
                         name="nome"
                         autoComplete="name"
                         autoFocus
                     />
+                    <h2 className={classes.texto} style={{ color: '#7A57EA' }}>
+                        E-mail
+                    </h2>
                     <TextField className={classes.campos}
                         variant="outlined"
                         margin="normal"
                         required
-                        fullWidth
                         id="email"
                         label="E-mail"
                         name="email"
                         autoComplete="email"
                         autoFocus
                     />
+                    <h2 className={classes.texto} style={{ color: '#7A57EA' }}>
+                        CPF
+                    </h2>
                     <TextField className={classes.campos}
                         variant="outlined"
                         margin="normal"
                         required
-                        fullWidth
                         id="cpf"
                         label="CPF"
                         name="cpf"
                         autoFocus
                     />
+                    <h2 className={classes.texto} style={{ color: '#7A57EA' }}>
+                        Senha
+                    </h2>
                     <TextField className={classes.campos}
                         variant="outlined"
                         margin="normal"
                         required
-                        fullWidth
                         name="senha"
                         label="Senha"
                         type="password"
                         id="senha"
                         autoComplete="new-password"
                     />
+                    <h2 className={classes.texto} style={{ color: '#7A57EA' }}>
+                        Confirme sua senha
+                    </h2>
                     <TextField className={classes.campos}
                         variant="outlined"
                         margin="normal"
                         required
-                        fullWidth
                         name="confirmaSenha"
                         label="Confirmar Senha"
                         type="password"
                         id="password"
                         autoComplete="new-password"
                     />
+
                     <Button
+                        onClick={() => cadastrarUsuario()}
                         size="medium"
                         type="submit"
                         variant="contained"
@@ -110,8 +142,9 @@ export default function Cadastrar() {
                     >
                         Cadastrar
                     </Button>
+
                 </form>
             </div>
-        </Container>
+        </div >
     );
 }
