aa9a6b6c407000099962207e504c1203998cd346
joão <joaomarcos70outlook.com>
2020-06-18 21:21:38 -0300

    implementando os formularios de cadastro gestor

---
+++ frontend/src/pages/Gestor/cadastro.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import CadastroGestor from '../../components/cadastroGestor/CadastroGestor'
+
+export default function cadastroGestor(){
+    return(
+        <CadastroGestor></CadastroGestor>
+    )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 
 import Inicio from './pages/teste/inicio'
 import TesteImagem from './pages/teste/testeImagem';
+import CadastroGestor from './pages/Gestor/cadastro'
 
 export default function Routes(){
     return(
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route exact path="/" component={Inicio}/>
                 <Route path="/teste" component={TesteImagem}/>
+                <Route path="/cadastrogestor" component={CadastroGestor}/>
             </Switch>
         </BrowserRouter>
         )
