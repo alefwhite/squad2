581ee11ec462076007f1703cb6a13398ec7c168f
joão <joaomarcos70outlook.com>
2020-06-19 09:29:42 -0300

    Refatorando Formularios de Cadastro Gestor

--- frontend/src/pages/Gestor/cadastro.js
+++ frontend/src/pages/Gestor/cadastro.js
@@ -1,8 +1,113 @@
-import React from 'react'
-import CadastroGestor from '../../components/cadastroGestor/CadastroGestor'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default function cadastroGestor(){
-    return(
-        <CadastroGestor></CadastroGestor>
-    )
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignIn() {
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="nome"
+                        label="Nome Completo"
+                        name="nome"
+                        autoComplete="name"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="E-mail"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="cpf"
+                        label="CPF"
+                        name="cpf"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="senha"
+                        label="Senha"
+                        type="password"
+                        id="senha"
+                        autoComplete="new-password"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="confirmaSenha"
+                        label="Comfirmar Senha"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Cadastrar
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+              </Link>
+                        </Grid>
+
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+    );
 }
