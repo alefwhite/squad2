4ed3e54b16f54cd3676a7de0f094469f744cf9e2
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-07-04 15:02:40 -0300

    alterando input de novo projeto

--- frontend/src/components/NovoProjeto/NovoProjeto.js
+++ frontend/src/components/NovoProjeto/NovoProjeto.js
@@ -92,7 +92,7 @@ function NovoProjeto({handleClose, setOpen}){
                 <h1 style={estilo.espacoCampo}><Input  funcao={(evento)=>handlePreencher(evento,"nome")} width="45vw" variant="standard"/></h1 >
 
                 <h1 style={estilo.espacoCampo}><InputData value={inicio}  funcao={(evento)=>handlePreencher(evento,"inicio")} style={estilo.input[0]} label="Início"/>
-                <InputData value={fim} funcao={(evento)=>handlePreencher(evento,"fim")} style={estilo.input[1]} label="Fim"/></h1 >
+                <InputData value={fim} width='30vw' funcao={(evento)=>handlePreencher(evento,"fim")} style={estilo.input[1]} label="Fim"/></h1 >
                 <div>
                 <p style={estilo.p}>Descrição do projeto</p>
                 <h1  style={estilo.espacoCampo}><Input funcao={(evento)=>handlePreencher(evento,"descricao")} multiline="false" width="35vw"/></h1 >
