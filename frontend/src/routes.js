eae863fda0e7fc5ca0252376001a4a18f4c5c5e9
Alef White <alefwhite@hotmail.com>
2020-07-08 22:48:20 -0300

    Incluindo permissão na rota /dashboard com o Luiz

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Login from './pages/Login/login';
 import Main from './pages/Main/main';
 import GestorCadastro from './pages/Gestor/cadastro';
 import FuncionarioCadastro from './pages/Funcionario/cadastrofunc';
+import { usuarioAutenticado } from './service/auth';
 
+const Permissao = ({ component: Component }) => (
+    <Route 
+        render={props => usuarioAutenticado() ? 
+                (
+                    <Component {...props} />
+                )                
+                :
+                (
+                    <Redirect to={{ pathname: "/" }} />
+                )
+        }
+    />
+)
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/gestor" component={GestorCadastro} />
                 <Route exact path="/" component={Login}/>
-                <Route exact path="/dashboard" component={Main} />
+                <Route exact path="/gestor" component={GestorCadastro} />
                 <Route exact path="/convite" component={FuncionarioCadastro} />
+                <Permissao exact path="/dashboard" component={Main} />
             </Switch>
         </BrowserRouter>
     )
